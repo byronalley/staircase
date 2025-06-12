435b423a29dbad971fa32fb107dec1dc17ebcb6d
Byron Alley <byron@byronalley.com>
2025-06-12 01:21:55 -0700

    Shorten

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() -> io::Result<()> {
     let mut text = String::new();
     io::stdin().read_to_string(&mut text)?;
 
-    println!("{}", staircase::staircase(text.as_str()));
+    println!("{}", staircase::staircase(&text));
 
     Ok(())
 }
